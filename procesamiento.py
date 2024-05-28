7b4d1d8c7de6a6faf1f3acdb114ce6d436be160a
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-05-27 20:23:23 -0500

    Terminado 2

--- procesamiento.py
+++ procesamiento.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import numpy as np
 
-pruebas = ["50", "100", "150", "200", "250", "300", "350", "400", "600", "700"]
+pruebas = ["50", "100", "150", "200", "250", "300", "350", "400", "600", "700", "1000", "1500"]
 noCumple = []
 totalDatos = []
 porcentaje = []
@@ -22,7 +22,7 @@ for i in pruebas:
     average.append(df[['elapsed']].mean().values[0])
 
 
-print(noCumple)
+
 # Convert pruebas to numeric values for regression
 pruebas_numeric = np.array([int(p) for p in pruebas])
 x_dense = np.linspace(min(pruebas_numeric), max(pruebas_numeric), 500)
@@ -33,14 +33,12 @@ slope, intercept, r_value, p_value, std_err = linregress(pruebas_numeric, porcen
 # Generate regression line values
 regression_line = slope * x_dense + intercept
 
-# Print the linear regression formula
-print(f'The formula of the linear regression line is: y = {slope:.2f}x + {intercept:.2f}')
 
 # Plot data
 plt.plot(pruebas_numeric, porcentaje, label='Data')
 
 # Plot regression line
-plt.plot(x_dense, regression_line, label='Linear Regression', color='green')
+plt.plot(x_dense, regression_line,linestyle='--', label=f'y = {slope:.2f}x + {intercept:.2f}', color='green')
 
 # Add horizontal line at y = 6%
 plt.axhline(y=6, color='r', linestyle='--', label='y = 6%')
@@ -48,12 +46,6 @@ plt.axhline(y=6, color='r', linestyle='--', label='y = 6%')
 # Add labels and title
 plt.xlabel('Numero de usuarios')
 plt.ylabel('Porcentaje de request que tiene un tiempo mayor a 4s')
-plt.title('Performance Analysis')
-
-# Add legend
+plt.title('Porcentaje de usuarios con tiempo de respuesta\nmayor a 4s  por numero total de usuarios')
 plt.legend()
-
-# Show plot
 plt.show()
-
-print(porcentaje)
